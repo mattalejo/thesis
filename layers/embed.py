67a30c4011fd6cfc26e7f7bed6d5bae48162aec4
Matt A <72350251+mattalejo@users.noreply.github.com>
2024-02-23 21:12:14 +0800

    some dimension fixing

--- layers/embed.py
+++ layers/embed.py
@@ -22,12 +22,12 @@ class PositionalEncoding(nn.Module):
     
 
 class Time2Vec(nn.Module):
-    def __init__(self, in_features, out_features):
+    def __init__(self, out_features):
         super(Time2Vec, self).__init__()
         self.out_features = out_features
-        self.w0 = nn.parameter.Parameter(torch.randn(in_features, 1))
+        self.w0 = nn.parameter.Parameter(torch.randn(1))
         self.b0 = nn.parameter.Parameter(torch.randn(1))
-        self.w = nn.parameter.Parameter(torch.randn(in_features, out_features-1))
+        self.w = nn.parameter.Parameter(torch.randn(out_features-1))
         self.b = nn.parameter.Parameter(torch.randn(out_features-1))
         self.f = torch.sin
 
