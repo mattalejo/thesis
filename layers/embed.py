326ffc8564094a6f9a7d478ac0352c03f64ac509
Matt A <72350251+mattalejo@users.noreply.github.com>
2024-02-25 15:25:33 +0800

    fix: positional encoding

--- layers/embed.py
+++ layers/embed.py
@@ -4,7 +4,7 @@ import math
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_len=5000):
+    def __init__(self, d_model, max_len=10000):
         super(PositionalEncoding, self).__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
