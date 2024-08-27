1bac0729451bcefd874036610240f30b4a901324
Matt A <72350251+mattalejo@users.noreply.github.com>
2024-08-28 00:57:08 +0800

    Update rnn.py

--- models/rnn.py
+++ models/rnn.py
@@ -9,7 +9,7 @@ class RNN(nn.Module):
         self, 
         input_size: int = 1,  # number of features
         hidden_size: int = 128, 
-        horizon: int = 1,
+        output_size: int = 1,
         num_layers: int = 4,
         dropout: float = 0.1,
         batch_first: bool = True
@@ -17,22 +17,21 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.model_type = "RNN"
         self.hidden_size = hidden_size
-        self.horizon = horizon  # Add horizon attribute
         self.rnn = nn.RNN(
             input_size=input_size, 
             hidden_size=hidden_size, 
             dropout=dropout,
             num_layers=num_layers
         )
-        self.fc = nn.Linear(hidden_size, horizon * input_size)
+        self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, src, src_time=None, tgt=None, tgt_time=None):  # x: (batch, seq_len, input_size)
         # h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)  # Initial hidden state
         src_time, tgt, tgt_time = None, None, None  # Garbage collection
         out, _ = self.rnn(src)  #, h0) 
         out = self.fc(out[:, -1:])  # , :])  # Get output from the last time step
+        out = out.view(-1, self.horizon, 1)
         return out
-
 
 class RNN5Day(nn.Module):
     """
