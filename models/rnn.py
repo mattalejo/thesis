b3463668ea95cf8f9ab5243a0d3174b884a35cc9
Matt A <72350251+mattalejo@users.noreply.github.com>
2024-08-27 21:24:12 +0800

    Update rnn.py

--- models/rnn.py
+++ models/rnn.py
@@ -23,7 +23,7 @@ class RNN(nn.Module):
             dropout=dropout,
             num_layers=num_layers
         )
-        self.fc = nn.Linear(hidden_size, )
+        self.fc = nn.Linear(hidden_size, horizon * input_size)
 
     def forward(self, src, src_time=None, tgt=None, tgt_time=None):  # x: (batch, seq_len, input_size)
         # h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)  # Initial hidden state
